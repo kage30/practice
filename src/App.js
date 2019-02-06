6df9ff7319feb98167803e32c30f0d385b17615a
kageyama <yuka.kageyama@nijibox.co.jp>
2019-02-07 01:11:13 +0900

    ざっくり部署選択とワード検索

--- src/App.js
+++ src/App.js
@@ -1,101 +1,159 @@
-import React, { Component } from 'react';
+import * as React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  request;
-  constructor() {
-    super();
-    this.state = {
-      isLogin: false,
-      departmentList: [],
-      user: "",
-    };
-  }
-  componentWillMount() {
-    this.request = axios.create({
-      baseURL: 'https://kadou.i.nijibox.net/api',
-      withCredentials: true,
-    })
+
+
+class App extends React.Component {
     
-    this.loadAuth()
-      .then(() => {
-        if (!this.state.isLogin) {
-          return Promise.resolve();
-        }
-        return this.loadDepartments();
-      })
-      .catch((err) => {
-        alert("APIがエラーを返しました\n\n" + err);
-      })
 
-      ;
-  }
-  commonResponseHandling(res) {
-    console.debug(res);
-    if (res.data.code !== "200") {
-      console.error(res.data.data);
-      return Promise.reject("API Error:" + res.data.data.message);
+    constructor(props){
+        super(props);
+
+        this.state = {
+            isLogin:false,
+            departmentList : [],
+            user:[],
+            // value:'',
+            word: '',
+            member:[]
+        };
     }
-    return Promise.resolve(res.data.data);
-  }
 
-  loadAuth() {
-    return this.request.get('/auth', { params: { callback: 'http://localhost:3000' } })
-      .then(this.commonResponseHandling)
-      .then(result => {
-        if (result.is_login) {
-          this.setState({ isLogin: true });
-        } else if (result.auth_url) {
-          window.location.href = result.auth_url;
+    componentDidMount() {
+        this.httpClient = axios.create({
+            baseURL:'https://kadou.i.nijibox.net/api',
+            withCredentials:true,
+        });
+
+
+        this.loadAuth()
+            .then(()=>{
+                if(! this.state.isLogin){
+                    return Promise.resolve();
+                }
+                return this.loadDepartments();
+            })
+            .catch((err)=>{
+                alert("APIがエラーを返しました\n\n" + err);
+            })
+
+        ;
+    }
+    loadAuth(){
+        return this.httpClient.get('/auth' , {params:{callback:'http://localhost:3000'}})
+            .then(this.commonResponseHandling)
+            .then((result)=>{
+                if(result.is_login){
+                    this.setState({isLogin:true});
+                }else if(result.auth_url){
+                    window.location.href = result.auth_url;
+                }
+            });
+    }
+    loadDepartments(){
+        return this.httpClient.get('/who/departments')
+            .then(this.commonResponseHandling)
+            .then((result)=>{
+                this.setState({departmentList : result});
+            })
+    }
+
+    commonResponseHandling(res){
+        console.debug(res);
+        if(res.data.code !== "200"){
+            console.error(res.data.data);
+            return Promise.reject("API Error:" + res.data.data.message);
         }
-      });
-  }
-  loadDepartments() {
-    return this.request.get('/who/departments')
-      .then(this.commonResponseHandling)
-      .then((result) => {
-        this.setState({ departmentList: result });
-      })
-  }
+        return Promise.resolve(res.data.data);
+    }
 
-  loadUser() {
-    return this.httpClient.get('/who/user/1')
-      .then(this.commonResponseHandling)
-      .then((result) => {
-        this.setState({ user: result });
-      })
-  }
+    loadUser(e){
+      return this.httpClient.get('/who/search', {params:{department_id: e.target.value}})
+          .then(this.commonResponseHandling)
+          .then((result)=>{
+            this.setState({user : result.item_list, page : result.summary.total_pages});
+            console.log(result.summary.total_pages);
+          })
+    }
 
-  render() {
-    return (
-      <div>
-        <ul>
-          {this.state.departmentList.map((row, index) => {
-            return <li key={index}>{row.department_name}</li>;
-          })}
-        </ul>
+    handleChange(word){
+      this.setState({word: word});
+    }
 
-        {this.state.isLogin ?
-          <div>
-            <button onClick={this.clickHandler}>ユーザも取得してみる</button>
+    searchUser(text){
+      text = this.state.word;
+      console.log(text);
+      return this.httpClient.get('/who/search', {params:{query: text}})
+          .then(this.commonResponseHandling)
+          .then((result)=>{
+            console.log(result);
+            this.setState({member:result.item_list});
+          })
+    }
 
-            {this.state.user &&
-              <div>
-                {this.state.user.user_name}<br />{this.state.user.description}
-                {/* <img src={this.state.user.main_photo_url} /> */}
-              </div>
-            }
-          </div>
-          :
-          <p>未ログイン</p>
-        }
+    clickHandler = ()=>{
+        this.loadUser()
+            .catch((err)=>{
+                alert('エラー発生');
+            });
+    };
 
 
-      </div>
-    );
-  }
+
+
+    render() {
+
+        return (
+            <div>
+                {/* <ul>
+                    {this.state.departmentList.map((row,index)=>{
+                        return <li key={index}>{row.department_name}</li>;
+                    })}
+                </ul> */}
+
+                <form>
+                  <label>
+                    部署を選択してください 
+                    <select onChange={e => this.loadUser(e)}>
+                    <option value='0'>選択してください</option>
+                    <option value='2'>MP事業部</option>
+                    <option value='3'>OS事業部</option>
+                    <option value='8'>UI/UX制作室</option>
+                    <option value='4'>開発室</option>
+                    <option value='5'>クリエイティブ室</option>
+                    <option value='6'>QAグループ</option>
+                    <option value='7'>経営企画室</option>
+                    <option value='1'>ニジボックス</option>
+                    </select>
+                  </label>
+                </form>
+
+                <input type="text" value={this.state.word} onChange={e => this.handleChange(e.target.value)}/>
+                <button onClick={e => this.searchUser(e)}>検索</button>
+
+                <div className='members'>
+                  {this.state.member.map((row,index)=>{
+                          return <div className='members_card' key={index}>
+                          {/* <p className='members_num'>User_id:{row.user_id}</p> */}
+                          <p className='members_name'>氏名:{row.user_name}</p>
+                          <img src={row.photo_url} alt='' />
+                          </div>;
+                      })}
+                </div>
+
+                <div className='members'>
+                  {this.state.user.map((row,index)=>{
+                          return <div className='members_card' key={index}>
+                          <p className='members_name'>氏名:{row.user_name}</p>
+                          <img src={row.photo_url} alt='' />
+                          </div>;
+                      })}
+                </div>
+
+            </div>
+        );
+    }
 }
 
 export default App;
